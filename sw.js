67a11452a3862b2f6c8a56e28412472013da6e4b
ANTON MAHOMEDOV <akitsatnafiya@gmail.com>
2026-05-25 02:43:22 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -13,15 +13,13 @@ const urlsToCache = [
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
 
