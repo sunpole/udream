a2e06c8d6a6d71b6d9b9c4bdb73cf75c2d032b25
ANTON MAHOMEDOV <akitsatnafiya@gmail.com>
2026-05-25 02:39:56 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = 'udream-v1';
+const urlsToCache = [
+  '/udream/',
+  '/udream/index.html',
+  '/udream/script.js',
+  '/udream/manifest.json',
+  '/udream/favicon.svg',
+  '/udream/icon-192.png',
+  '/udream/icon-512.png',
+  '/udream/apple-touch-icon.png',
+  '/udream/data/bd2.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+    ))
+  );
+});
